ff11e7912d8c3c52e607b3b04e029d8fc795518b
diego garcia vega <forlan_vega@hotmail.com>
2022-04-10 22:52:49 -0500

    Diseño

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,16 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.BarraHerramientas = new System.Windows.Forms.Panel();
             this.btnMinimizar = new System.Windows.Forms.Button();
             this.btnCerrarPrograma = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.PanelOpciones = new HMI_V2.Controles.EllipseControls();
             this.ellipseComponents1 = new HMI_V2.Componentes.EllipseComponents();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.BarraHerramientas.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // BarraHerramientas
             // 
             this.BarraHerramientas.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(150)))), ((int)(((byte)(150)))), ((int)(((byte)(255)))));
+            this.BarraHerramientas.Controls.Add(this.pictureBox1);
             this.BarraHerramientas.Controls.Add(this.btnMinimizar);
             this.BarraHerramientas.Controls.Add(this.btnCerrarPrograma);
             this.BarraHerramientas.Dock = System.Windows.Forms.DockStyle.Top;
@@ -50,6 +57,8 @@
             // btnMinimizar
             // 
             this.btnMinimizar.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnMinimizar.FlatAppearance.BorderSize = 0;
+            this.btnMinimizar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnMinimizar.Location = new System.Drawing.Point(701, 0);
             this.btnMinimizar.Name = "btnMinimizar";
             this.btnMinimizar.Size = new System.Drawing.Size(57, 40);
@@ -73,23 +82,60 @@
             this.btnCerrarPrograma.UseVisualStyleBackColor = true;
             this.btnCerrarPrograma.Click += new System.EventHandler(this.btnCerrarPrograma_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(89, 40);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // PanelOpciones
+            // 
+            this.PanelOpciones.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(150)))), ((int)(((byte)(150)))), ((int)(((byte)(255)))));
+            this.PanelOpciones.CornerRadius = 90;
+            this.PanelOpciones.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PanelOpciones.Location = new System.Drawing.Point(30, 30);
+            this.PanelOpciones.Margin = new System.Windows.Forms.Padding(50);
+            this.PanelOpciones.Name = "PanelOpciones";
+            this.PanelOpciones.Size = new System.Drawing.Size(740, 350);
+            this.PanelOpciones.TabIndex = 1;
+            this.PanelOpciones.Text = "ellipseControls1";
+            // 
             // ellipseComponents1
             // 
             this.ellipseComponents1.CornerRadius = 60;
             this.ellipseComponents1.TargetControl = this;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.PanelOpciones);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 40);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(30);
+            this.panel1.Size = new System.Drawing.Size(800, 410);
+            this.panel1.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.BarraHerramientas);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
             this.BarraHerramientas.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -100,6 +146,9 @@
         private System.Windows.Forms.Panel BarraHerramientas;
         private System.Windows.Forms.Button btnCerrarPrograma;
         private System.Windows.Forms.Button btnMinimizar;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private Controles.EllipseControls PanelOpciones;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
